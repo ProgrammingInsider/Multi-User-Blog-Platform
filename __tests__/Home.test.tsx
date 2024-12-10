dedb8ffa8c6d9e7177792cde414b560f43588ae2
Amanuel Abera <amanuelabera46@gmail.com>
2024-12-10 22:08:42 +0300

    Login and Registeration Form is created

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,8 @@
+import {render, screen} from "@testing-library/react"
+import Home from "@/app/page"
+
+describe('test it', ()=>{
+    it('test 1',() => {
+        expect(1 + 1).toBe(2);
+    })
+})

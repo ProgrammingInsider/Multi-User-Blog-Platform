19130f6ff2f852dc7085723d518f3383dc896baf
Amanuel Abera <amanuelabera46@gmail.com>
2024-12-10 21:51:01 +0300

    Login and Registeration Form is created

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,16 +1,2 @@
 import {render, screen} from "@testing-library/react"
 import Home from "@/app/page"
-
-describe('first', () => { 
-
-    it('should render home text', () => {
-        render(<Home/>)  //ARRANGE
-
-        const myElem = screen.getByText('Home') //ACT
-
-        expect(myElem).toBeInTheDocument(); //ASSERTION
-
-        
-    })
-
- })
